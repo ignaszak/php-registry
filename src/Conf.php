4ce3826cffb185e86e3c420c8008abc81647849a
ignaszak <tomek.ignaszak@gmail.com>
2018-04-23 22:34:03 +0200

    Update Conf.php

--- src/Conf.php
+++ src/Conf.php
@@ -1,21 +1,9 @@
 <?php
-/**
- * phpDocumentor
- *
- * PHP Version 7.0
- *
- * @copyright 2016 Tomasz Ignaszak
- * @license   http://www.opensource.org/licenses/mit-license.php MIT
- */
+
 declare(strict_types=1);
 
 namespace Ignaszak\Registry;
 
-/**
- *
- * @author Tomasz Ignaszak <tomek.ignaszak@gmail.com>
- *
- */
 abstract class Conf
 {
 
